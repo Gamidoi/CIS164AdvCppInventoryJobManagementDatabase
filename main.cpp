5dfed343f623cbd980faa03651c9477f186333e0
Sam Hapke <gamidoi@gmail.com>
2026-03-30 08:52:22 -0500

    first and very basic connection to the database.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+
+using namespace std;
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/statement.h>
+#include <iostream>
+
+bool testMySQLConnection() {
+	try {
+		// note, that I am not loading the password to github. Because of security.
+		// in my next update, I will probably prompt the user for the password.
+		string password = "thisIsNotReallyThePassword";
+		sql::mysql::MySQL_Driver *driver;
+		sql::Connection *con;
+
+		driver = sql::mysql::get_mysql_driver_instance();
+		con = driver->connect("localhost:3306", "root", password);
+
+		sql::Statement *stmt = con->createStatement();
+		stmt->execute("use inventoryManagement;");
+		stmt->execute(
+			"INSERT into Item (itemName, itemDescription, itemValue, itemQuantity, isRawResource) VALUES ('nails', 'basic iron nails', 0.01, 300, true)"
+			);
+		return true;
+
+	} catch (sql::SQLException &e) {
+		cout << e.what() << endl;
+		return false;
+	}
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	if (testMySQLConnection()) {
+		cout << "Database conneciton is good" << endl;
+	} else {
+		cout << "failed to connect to database" << endl;
+	}
 	return 0;
 }
