38ec27ee912bfd444a1bcfbd38a8e7133e90a186
Sam Hapke <gamidoi@gmail.com>
2026-05-03 19:06:55 -0500

    transfer

--- objects/Job.cpp
+++ objects/Job.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Job.h"
+
+string Job::getSQLInsert() {
+    string sqlInsert = "INSERT INTO Job (customerID) VALUES (";
+    sqlInsert += to_string(customerID);
+    sqlInsert += ");";
+    return sqlInsert;
+}
+string Job::getSQLWorkOrderInsert(string JobNumber){
+    string sqlInsert = "INSERT INTO WorkOrder (workOrderJobNumber, workOrderOrderedItem, workOrderItemQuantity) VALUES (";
+    sqlInsert += "(" + JobNumber + ", " + to_string(productID1) + ", " + to_string(quantity1) + ")";
+    sqlInsert += to_string(productID2) + ", " + to_string(quantity2) + ", ";
+    sqlInsert += to_string(productID3) + ", " + to_string(quantity3) + ", ";
+    sqlInsert += to_string(productID4) + ", " + to_string(quantity4) + ", ";
+    sqlInsert += to_string(productID5) + ", " + to_string(quantity5) + ", ";
+    sqlInsert += to_string(productID6) + ", " + to_string(quantity6) + ", ";
+    sqlInsert += to_string(productID7) + ", " + to_string(quantity7) + ", ";
+    sqlInsert += to_string(productID8) + ", " + to_string(quantity8);
+    return sqlInsert;
+}
--- objects/Job.h
+++ objects/Job.h
@@ -5,8 +5,72 @@
 #ifndef FINALPROJECTCIS164INVENTORYJOBMANAGEMENTDATABASE_JOB_H
 #define FINALPROJECTCIS164INVENTORYJOBMANAGEMENTDATABASE_JOB_H
 
+using namespace std;
+#include <string>
 
 class Job {
+private:
+    int customerID;
+    int productID1;
+    int quantity1;
+    int productID2;
+    int quantity2;
+    int productID3;
+    int quantity3;
+    int productID4;
+    int quantity4;
+    int productID5;
+    int quantity5;
+    int productID6;
+    int quantity6;
+    int productID7;
+    int quantity7;
+    int productID8;
+    int quantity8;
+public:
+    Job() {
+        customerID = productID1 = quantity1 = productID2 = quantity2 = productID3 = quantity3 =
+        productID4 = quantity4 = productID5 = quantity5 = productID6 = quantity6 = productID7 =
+        quantity7 = productID8 = quantity8 = 0;
+    }
+    string getSQLInsert();
+    string getSQLWorkOrderInsert(string JobNumber);
+
+    void setCustomerID(int id) {customerID = id;}
+    void setProductID1(int id) {productID1 = id;}
+    void setProductID2(int id) {productID2 = id;}
+    void setProductID3(int id) {productID3 = id;}
+    void setProductID4(int id) {productID4 = id;}
+    void setProductID5(int id) {productID5 = id;}
+    void setProductID6(int id) {productID6 = id;}
+    void setProductID8(int id) {productID8 = id;}
+    void setProductID7(int id) {productID7 = id;}
+    void setQuantity1(int quantity) {quantity1 = quantity;}
+    void setQuantity2(int quantity) {quantity2 = quantity;}
+    void setQuantity3(int quantity) {quantity3 = quantity;}
+    void setQuantity4(int quantity) {quantity4 = quantity;}
+    void setQuantity5(int quantity) {quantity5 = quantity;}
+    void setQuantity6(int quantity) {quantity6 = quantity;}
+    void setQuantity7(int quantity) {quantity7 = quantity;}
+    void setQuantity8(int quantity) {quantity8 = quantity;}
+
+    int getCustomerID() {return customerID;}
+    int getProductID1() {return productID1;}
+    int getProductID2() {return productID2;}
+    int getProductID3() {return productID3;}
+    int getProductID4() {return productID4;}
+    int getProductID5() {return productID5;}
+    int getProductID6() {return productID6;}
+    int getProductID7() {return productID7;}
+    int getProductID8() {return productID8;}
+    int getQuantity1() {return quantity1;}
+    int getQuantity2() {return quantity2;}
+    int getQuantity3() {return quantity3;}
+    int getQuantity4() {return quantity4;}
+    int getQuantity5() {return quantity5;}
+    int getQuantity6() {return quantity6;}
+    int getQuantity7() {return quantity7;}
+    int getQuantity8() {return quantity8;}
 };
 
 
